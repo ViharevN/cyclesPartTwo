829200bee928e9b5eefb1a29bc19080db9c0a550
ViharevN <swix_off@mail.ru>
2022-10-14 20:23:24 +0300

    1 задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //циклы часть 2
+        //домашнее задание 1
+
+
+        int total = 0;
+        int salary = 15_000;
+        int months = 0;
+        while (total <= 2_459_000) {
+            total = total + (total/100);
+            total = salary + total;
+            months = months + 1;
+            System.out.println(months + " месяц");
+            System.out.println(total + " кэш");
+            System.out.println("Месяц " + months + " , сумма накоплений равна " + total + " рублей");
+        }
     }
 }
